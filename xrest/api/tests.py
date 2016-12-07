30515661a6e859db0c0b47a748ac0b7ba5ca7b2c
freenoth <nothscr@gmail.com>
2016-12-07 10:31:44 +0500

    bad import from django 1.10 -> goes 1.9 style

--- xrest/api/tests.py
+++ xrest/api/tests.py
@@ -1,6 +1,6 @@
 # from django.test import TestCase
 
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
